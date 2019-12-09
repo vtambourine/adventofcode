329268c57893fa91bb0fe9f035ca3f68b37f248d
Benjamin T <vtambourine@ya.ru>
2019-12-09 17:15:10 +0100

    Day 3. Complete

--- day03/wires.js
+++ day03/wires.js
@@ -1,73 +1,64 @@
+function parseInput(input) {
+  return input.split("\n").map(wire => wire.split(","));
+}
+
+function manhattanDistance(x, y) {
+  return Math.abs(x) + Math.abs(y);
+}
+
 function intersection(input) {
-  const wires = input.split("\n").map(wire => wire.split(","));
-  let [l, r, t, b] = [0, 0, 0, 0];
-  let [x, y] = [0, 0];
-  wires.forEach(wire => {
-    [x, y] = [0, 0];
-    wire.forEach(step => {
-      const direction = step[0];
-      const value = parseInt(step.slice(1), 10);
-      switch (direction) {
-        case "R":
-          x += value;
-          r = Math.max(r, x);
-          break;
-        case "L":
-          x -= value;
-          l = Math.min(l, x);
-          break;
-        case "U":
-          y += value;
-          t = Math.max(t, y);
-          break;
-        case "D":
-          y -= value;
-          b = Math.min(b, y);
-          break;
-      }
-    });
-  });
-  const [width, height] = [
-    Math.abs(l) + Math.abs(r) + 1,
-    Math.abs(t) + Math.abs(b) + 1,
-  ];
-  const [m, n] = [width - r - 1, t];
-  const circuit = new Array(height).fill(0).map(() => new Array(width).fill(0));
-  circuit[n][m] = "X";
+  const wires = parseInput(input);
+  const circuit = {};
   let distance = Infinity;
-  wires.forEach((wire, k) => {
-    let [x, y] = [m, n];
+  wires.forEach((wire, n) => {
+    let [x, y] = [0, 0];
     wire.forEach(step => {
       const direction = step[0];
       const value = parseInt(step.slice(1), 10);
       for (let i = 0; i < value; i++) {
-        switch (direction) {
-          case "R":
-            x++;
-            break;
-          case "L":
-            x--;
-            break;
-          case "U":
-            y--;
-            break;
-          case "D":
-            y++;
-            break;
-        }
-        circuit[y][x] = circuit[y][x] | (k + 1);
-        if (circuit[y][x] >= 3) {
-          distance = Math.min(distance, Math.abs(m - x) + Math.abs(n - y));
+        let [dx, dy] = [
+          direction === "R" ? 1 : direction === "L" ? -1 : 0,
+          direction === "U" ? 1 : direction === "D" ? -1 : 0,
+        ];
+        [x, y] = [x + dx, y + dy];
+        const address = `${x}:${y}`;
+        circuit[address] = circuit[address] | (n + 1);
+        if (circuit[address] === 3) {
+          distance = Math.min(distance, manhattanDistance(x, y));
         }
       }
     });
   });
-
-  // circuit.forEach(row => {
-  //   console.log(row.join(" "));
-  // });
-
   return distance;
 }
 
-module.exports = { intersection };
+function length(input) {
+  const wires = parseInput(input);
+  const circuit = {};
+  let length = Infinity;
+  wires.forEach((wire, n) => {
+    let [x, y] = [0, 0];
+    let steps = 0;
+    wire.forEach(step => {
+      const direction = step[0];
+      const value = parseInt(step.slice(1), 10);
+      for (let i = 0; i < value; i++) {
+        steps++;
+        let [dx, dy] = [
+          direction === "R" ? 1 : direction === "L" ? -1 : 0,
+          direction === "U" ? 1 : direction === "D" ? -1 : 0,
+        ];
+        [x, y] = [x + dx, y + dy];
+        const address = `${x}:${y}`;
+        if (n === 0) {
+          circuit[address] = circuit[address] || steps;
+        } else if (circuit[address]) {
+          length = Math.min(length, circuit[address] + steps);
+        }
+      }
+    });
+  });
+  return length;
+}
+
+module.exports = { intersection, length };
--- day03/wires.test.js
+++ day03/wires.test.js
@@ -1,37 +1,59 @@
-const fs = require("fs");
-const path = require("path");
 const assert = require("assert");
-const { intersection } = require("./wires");
 
-suite("Day 3: Wires", () => {
-  test("part one", () => {
-    [
-      ["R2,U2\nU2,R2", 4],
-      ["R8,U5,L5,D3\nU7,R6,D4,L4", 6],
+const { intersection, length } = require("./wires");
+const { fetchInput } = require("../utils");
+
+suite("Day 3: Crossed Wires", () => {
+  suite("Part 1", () => {
+    test("Test cases are valid", () => {
       [
-        "R75,D30,R83,U83,L12,D49,R71,U7,L72\n" +
-          "U62,R66,U55,R34,D71,R55,D58,R83",
-        159,
-      ],
-      [
-        "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n" +
-          "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
-        135,
-      ],
-    ].forEach(([input, expected]) => {
-      assert.equal(intersection(input), expected);
+        ["R2,U2\nU2,R2", 4],
+        ["R8,U5,L5,D3\nU7,R6,D4,L4", 6],
+        [
+          "R75,D30,R83,U83,L12,D49,R71,U7,L72\n" +
+            "U62,R66,U55,R34,D71,R55,D58,R83",
+          159,
+        ],
+        [
+          "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n" +
+            "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+          135,
+        ],
+      ].forEach(([input, expected]) => {
+        assert.equal(intersection(input), expected);
+      });
+    });
+
+    const expectedAnswer = 1519;
+    test(`Answer is ${expectedAnswer}`, () => {
+      const input = fetchInput();
+      assert.equal(intersection(input), expectedAnswer);
     });
   });
 
-  test("answer", () => {
-    const input = fs.readFileSync(
-      path.join(__dirname, path.basename(__dirname) + ".input"),
-      {
-        encoding: "utf8",
-      }
-    );
-    assert.doesNotThrow(() => {
-      console.log("Answer is:", intersection(input));
+  suite("Part 2", () => {
+    test("Test cases are valid", () => {
+      [
+        ["R8,U5,L5,D3\nU7,R6,D4,L4", 30],
+        [
+          "R75,D30,R83,U83,L12,D49,R71,U7,L72\n" +
+            "U62,R66,U55,R34,D71,R55,D58,R83",
+          610,
+        ],
+        [
+          "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n" +
+            "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",
+          410,
+        ],
+      ].forEach(([input, expected]) => {
+        assert.equal(length(input), expected);
+      });
+    });
+
+    const expectedAnswer = 14358;
+    test(`Answer is ${expectedAnswer}`, () => {
+      const input = fetchInput();
+      assert.equal(length(input), expectedAnswer);
     });
   });
 });
