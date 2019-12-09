a8707dfb03221f91cb3891f47ca54ed33c79f9e4
Benjamin T <vtambourine@ya.ru>
2019-12-09 09:05:02 +0100

    Common function for fetch an input for the day

---
+++ utils/index.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
+function fetchInput() {
+  const projectName = path
+    .resolve(__dirname, "..")
+    .split(path.sep)
+    .reverse()[0];
+
+  const stack = new Error().stack;
+  const dayName = stack
+    .split("at ")[2]
+    .trim()
+    .match(`${projectName}\/([0-9a-z]+)\/`)[1];
+
+  const input = fs.readFileSync(
+    path.resolve(process.cwd(), dayName, `${dayName}.input`),
+    {
+      encoding: "utf8",
+    }
+  );
+
+  return input;
+}
+
+module.exports = {
+  fetchInput,
+};
