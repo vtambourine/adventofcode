3c51e26462e5164edcf3f713739045ade0b2477a
Benjamin T <vtambourine@ya.ru>
2019-12-10 19:05:05 +0100

    Day 8. Draft

--- day08/image.js
+++ day08/image.js
@@ -1,14 +1,12 @@
-function image(input) {
-  const [width, height] = [25, 6];
-
+function validate(input, { width, height } = { width: 25, height: 6 }) {
   let zeros = 0;
-  let minZeros = Infinity;
+  let minimum = Infinity;
   let layer = 0;
   for (let d = 0; d < input.length; d++) {
     if (d && d % (width * height) === 0) {
-      if (zeros < minZeros) {
+      if (zeros < minimum) {
         layer = d - width * height;
-        minZeros = zeros;
+        minimum = zeros;
       }
       zeros = 0;
     }
@@ -25,4 +23,25 @@ function image(input) {
   return counts[1] * counts[2];
 }
 
-module.exports = { image };
+function image(input, { width, height } = { width: 25, height: 6 }) {
+  let result = new Array(width * height).fill("2");
+  for (let d = 0; d < input.length; d++) {
+    let i = d % (width * height);
+    console.log(d, input[d], i, result[i]);
+    if (result[i] === "2") {
+      result[i] = input[d];
+    }
+  }
+
+  let out = "";
+  for (let d = 0; d < result.length; d++) {
+    if (d % width === 0) {
+      console.log(out);
+      out = "";
+    }
+    out += result[d] === "0" ? " " : "X";
+  }
+  console.log(out);
+}
+
+module.exports = { validate, image };
--- day08/image.test.js
+++ day08/image.test.js
@@ -1,18 +1,32 @@
 const assert = require("assert");
 
-const { image } = require("./image");
+const { validate, image } = require("./image");
 const { fetchInput } = require("../utils");
 
 suite("Day 8: Space Image Format", () => {
   suite("Part 1", () => {
     test("Test cases are valid", () => {
       [["123456789012", 1]].forEach(([input, expected]) => {
-        assert.equal(image(input), expected);
+        assert.equal(validate(input, { width: 3, height: 2 }), expected);
       });
     });
 
     const expectedAnswer = 2562;
-    test.only(`Answer is ${expectedAnswer}`, () => {
+    test(`Answer is ${expectedAnswer}`, () => {
+      const input = fetchInput();
+      assert.equal(validate(input), expectedAnswer);
+    });
+  });
+
+  suite("Part 2", () => {
+    test("Test cases are valid", () => {
+      [["0222112222120000", "0110"]].forEach(([input, expected]) => {
+        assert.equal(image(input, { width: 2, height: 2 }), expected);
+      });
+    });
+
+    const expectedAnswer = "ZFLBY";
+    test(`Answer is ${expectedAnswer}`, () => {
       const input = fetchInput();
       assert.equal(image(input), expectedAnswer);
     });
