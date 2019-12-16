75a612a34f8839bbf2afa175554bfa65444185da
Benjamin T <vtambourine@ya.ru>
2019-12-16 18:38:16 +0100

    Day 9. Draft

--- day09/intcode.js
+++ day09/intcode.js
@@ -3,7 +3,10 @@ class Computer {
     this.base = 0;
     this.codes = codes;
     this.cursor = 0;
-    this.memory = {};
+    this.memory = codes.reduce((memory, code, index) => {
+      memory[index] = code;
+      return memory;
+    }, {});
     this.output = 0;
   }
 
@@ -22,90 +25,82 @@ class Computer {
 
   read(count, modes) {
     const { base, codes, cursor, memory } = this;
-    console.log(cursor, codes, modes);
+    // console.log(cursor, codes, modes);
     return new Array(count).fill(0).map((value, index) => {
       let address;
       switch (modes[index]) {
-        case 0: // Position mode
-        case undefined:
-          address = codes[cursor + index + 1];
-          if (address >= codes.length) {
-            address = address - codes.length;
-            memory[String(address)] = memory[String(address)] || 0;
-            return memory[String(address)];
-          } else {
-            return codes[codes[cursor + index + 1]];
-          }
-        case 1: // Immediate mode
-          return codes[cursor + index + 1];
         case 2: // Relative mode
           address = base + codes[cursor + index + 1];
-          if (address >= codes.length) {
-            address = address - codes.length;
-            memory[String(address)] = memory[String(address)] || 0;
-            return memory[String(address)];
-          } else {
-            return codes[address];
-          }
+          memory[address] = memory[address] || 0;
+          return memory[address];
+        case 1: // Immediate mode
+          return codes[cursor + index + 1];
+        case 0: // Position mode
+        default:
+          address = codes[cursor + index + 1];
+          memory[address] = memory[address] || 0;
+          return memory[address];
       }
     });
   }
 
-  write(address, value) {}
+  address(index, modes) {
+    const { base, codes, cursor, memory } = this;
+    const mode = modes[index] || 0;
+    let address;
+    switch (mode) {
+      case 2: // Relative mode
+        address = base + codes[cursor + index];
+        return address;
+      case 1:
+        throw new Error("Cannot read address in immediate mode");
+      case 0: // Position mode
+        address = codes[cursor + index];
+        // console.log("> mode", address, memory["" + address], "=", mode, memory);
+        return address;
+    }
+  }
+
+  write(address, value) {
+    this.map[address] = value;
+  }
 
   *run() {
     const { base, cursor, codes, memory } = this;
     while (this.cursor < this.codes.length) {
-      let { operator, modes } = Computer.command(codes[cursor]);
+      let { operator, modes } = Computer.command(codes[this.cursor]);
       let address;
       let parameters = [];
+      let target;
       switch (operator) {
         case 1: // Addition
-          parameters = this.read(3, modes);
-          // this.write(
-          //   parameters[2],
-          //   parameters.reduce((result, value) => result + value, 0)
-          // );
-          address = codes[cursor + 3];
-          // if (address >= codes.length) {
-          //   address -= codes.length;
-          //   memory[String(address)] = parameters.reduce(
-          //     (result, value) => result + value,
-          //     0
-          //   );
-          // } else {
-          codes[parameters[2]] = parameters.reduce(
+          parameters = this.read(2, modes);
+          memory[this.address(3, modes)] = parameters.reduce(
             (result, value) => result + value,
             0
           );
-          // }
+          console.log("sum", parameters, this.address(3, modes));
           this.cursor += 4;
           break;
         case 2: // Multiplication
-          parameters = this.read(3, modes);
-          codes[parameters[2]] = parameters.reduce(
+          parameters = this.read(2, modes);
+          memory[this.address(3, modes)] = parameters.reduce(
             (result, value) => result * value,
             1
           );
-          console.log(codes);
           this.cursor += 4;
-          // parameters = this.read(2, modes);
-          // codes[codes[cursor + 3]] = parameters.reduce(
-          //   (result, value) => result * value,
-          //   1
-          // );
-          // this.cursor += 4;
           break;
         case 3: // Input
           let input = yield;
-          codes[codes[cursor + 1]] = input;
+          codes[this.address(1, modes)] = input;
           this.cursor += 2;
           break;
         case 4: // Output
           parameters = this.read(1, modes);
           this.output = parameters[0];
-          yield output;
+          yield this.output;
           this.cursor += 2;
+          console.log("<", memory);
           break;
         case 5: // Jump if true
           parameters = this.read(2, modes);
@@ -118,13 +113,16 @@ class Computer {
           break;
         case 7: // Less than
           parameters = this.read(2, modes);
-          codes[codes[cursor + 3]] = parameters[0] < parameters[1] ? 1 : 0;
+          memory[this.address(3, modes)] =
+            parameters[0] < parameters[1] ? 1 : 0;
           this.cursor += 4;
           break;
         case 8: // Equals
           parameters = this.read(2, modes);
-          codes[codes[cursor + 3]] = parameters[0] === parameters[1] ? 1 : 0;
+          memory[this.address(3, modes)] =
+            parameters[0] === parameters[1] ? 1 : 0;
           this.cursor += 4;
+          console.log("equals !", parameters, memory);
           break;
         case 9: // Adjust relative base
           parameters = this.read(1, modes);
@@ -133,7 +131,7 @@ class Computer {
           // console.log("adjust !", base);
           break;
         case 99:
-          console.log("Done", output);
+          console.log("Done", this.output);
           return this.output;
         default:
           throw new Error("Unknown operator code: " + operator);
