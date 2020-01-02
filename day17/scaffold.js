7f3cf8ccc49a032587b2ff1d75d2d9ea80b30e6a
Benjamin T <vtambourine@ya.ru>
2020-01-02 21:17:37 +0100

    Day 17. Complete

--- day17/scaffold.js
+++ day17/scaffold.js
@@ -1,109 +1,209 @@
-const readline = require("readline");
 const { Computer } = require("../intcode/intcode");
 
 function parseInput(input) {
   return input.split(",").map(Number);
 }
 
-const SCAFFOLD = "#";
+class Scaffold {
+  static SCAFFOLD = "#";
+
+  static TOP = 1;
+  static BOTTOM = 2;
+  static LEFT = 3;
+  static RIGHT = 4;
+
+  static DIRECTIONS = [
+    [0, 0], // center
+    [0, -1], // top
+    [0, 1], // bottom
+    [-1, 0], // left
+    [1, 0], // right
+  ];
+
+  static TURN_BACK = [
+    0,
+    Scaffold.BOTTOM,
+    Scaffold.TOP,
+    Scaffold.RIGHT,
+    Scaffold.LEFT,
+  ];
+
+  static TURN_LEFT = [
+    0,
+    Scaffold.LEFT,
+    Scaffold.RIGHT,
+    Scaffold.BOTTOM,
+    Scaffold.TOP,
+  ];
+
+  static TURN_RIGHT = [
+    0,
+    Scaffold.RIGHT,
+    Scaffold.LEFT,
+    Scaffold.TOP,
+    Scaffold.BOTTOM,
+  ];
+
+  static BOTS = ["^", "v", "<", ">"];
+
+  constructor(screen) {
+    this.screen = screen.split("\n").map(row => row.split(""));
+
+    this.width = this.screen[0].length;
+    this.height = this.screen.length;
+    this.list = this.screen.reduce((row, nextRow) => row.concat(nextRow));
+
+    this.position = this.list.findIndex(segment =>
+      Scaffold.BOTS.includes(segment)
+    );
+    this.direction = Scaffold.BOTS.indexOf(this.list[this.position]) + 1;
+
+    this.route = [];
+  }
+
+  index([x, y]) {
+    if (x < 0 || x >= this.width || y < 0 || y >= this.height) {
+      return -1;
+    }
+    return this.width * y + x;
+  }
+
+  coordinates(index) {
+    return [index % this.width, Math.floor(index / this.width)];
+  }
+
+  neighbours(index) {
+    const [x, y] = this.coordinates(index);
+
+    return Scaffold.DIRECTIONS.map(([dx, dy]) => {
+      const next = this.index([x + dx, y + dy]);
+      if (Scaffold.BOTS.includes(this.list[next])) {
+        return Scaffold.SCAFFOLD;
+      } else {
+        return this.list[next];
+      }
+    });
+  }
+
+  end() {
+    const lookup = this.neighbours(this.position);
+    lookup.splice(Scaffold.TURN_BACK[this.direction], 1);
+    lookup.splice(0, 1);
+    return lookup.every(segment => segment !== Scaffold.SCAFFOLD);
+  }
+
+  move() {
+    let [x, y] = this.coordinates(this.position);
+    let [dx, dy] = Scaffold.DIRECTIONS[this.direction];
+
+    let rx, ry;
+    if (
+      (([rx, ry] = [dy, -dx]),
+      this.list[this.index([x + rx, y + ry])] === Scaffold.SCAFFOLD)
+    ) {
+      this.route.push("L");
+      this.direction = Scaffold.TURN_LEFT[this.direction];
+    } else if (
+      (([rx, ry] = [-dy, dx]),
+      this.list[this.index([x + rx, y + ry])] === Scaffold.SCAFFOLD)
+    ) {
+      this.route.push("R");
+      this.direction = Scaffold.TURN_RIGHT[this.direction];
+    }
+
+    let steps = 0;
+    [dx, dy] = Scaffold.DIRECTIONS[this.direction];
+    let g = 1e4;
+    while (
+      g-- &&
+      this.neighbours(this.position)[this.direction] === Scaffold.SCAFFOLD
+    ) {
+      steps++;
+      [x, y] = [x + dx, y + dy];
+      this.position = this.index([x, y]);
+    }
+    this.route.push(steps);
+  }
+
+  traverse() {
+    while (!this.end()) {
+      this.move();
+    }
+    return this.route;
+  }
+
+  alignment() {
+    return this.list.reduce(
+      (res, _, index) =>
+        res +
+        (this.neighbours(index).every(segment => segment === Scaffold.SCAFFOLD)
+          ? this.coordinates(index).reduce((x, y) => x * y)
+          : 0),
+      0
+    );
+  }
+}
 
 function alignment(program) {
   const codes = parseInput(program);
+
   let output = "";
-  const computer = new Computer(codes).run(
+  new Computer(codes).run(
     () => {},
     char => {
       output += String.fromCharCode(char);
     }
   );
 
-  // output =
-  //   "..#..........\n" +
-  //   "..#..........\n" +
-  //   "#######...###\n" +
-  //   "#.#...#...#.#\n" +
-  //   "#############\n" +
-  //   "..#...#...#..\n" +
-  //   "..#####...^..";
-
-  console.log(output);
-
-  const screen = output.split("\n").map(row => row.split(""));
-  const width = screen[0].length;
-  const height = screen.length;
-
-  let alignments = 0;
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      if (
-        screen[y][x] === SCAFFOLD &&
-        screen[y - 1] &&
-        screen[y - 1][x] === SCAFFOLD &&
-        screen[y + 1] &&
-        screen[y + 1][x] === SCAFFOLD &&
-        screen[y][x - 1] === SCAFFOLD &&
-        screen[y][x + 1] === SCAFFOLD
-      ) {
-        screen[y][x] = "O";
-        console.log("a:", x, y, x * y);
-        alignments += x * y;
-      }
-    }
-
-    // console.log(screen[y]);
-  }
-
-  console.log(screen.map(row => row.join(" ")).join("\n"));
-
-  return alignments;
+  return new Scaffold(output).alignment();
 }
 
-module.exports = { alignment };
+const ZIP_REGEXP = /^(.{1,21})\1*?(.{1,21})(?:\1|\2)*?(.{1,21})(?:\1|\2|\3)*$/;
 
-/**
- . . . . . . . . . . . . . . . . . . . . B B B B B B B B B B B B ^ . . . .
- . . . . . . . . . . . . . . . . . . . . #12. . . . . . . . . . .-1. . . .
- . . . . . . . . . . . . . . . . A A A A A A A . . . . . . . . . . . . . .
- . . . . . . . . . . . . . . . . A . . . # . A . . . . . . . . . . . . . .
- . . . . . . . . . . . . . . . . A . . . # . A . . . . . . . . . . . . . .
- . . . . . . . . . . . . . . . . A . . . # . A . . . . . . . . . . . . . .
- . . . . . . . . . . . . . . . . A . A A A A A . . . . . . . . . . . . . .
- . . . . . . . . . . 1-. . 4 . 7 A . B . # .-13. . . . . . . . . . . . . .
- . . . . . . . . . . B B B B B B B B B B B B # . . . . . . . . . . . . . .
- . . . . . . . . . . # . . . . . A . B . A . # . . . . . . . . . . . . . .
- . . . . . . . . . . # . . . . . A . # . A . # . . . . . . . . . . . . . .
- . . . . . . . . . . # . . . . . A . B . A . # . . . . . . . . . . . . . .
- . . . . . . . . . . # . . . . . A A A A A . # . . . . . . . . . . . . . .
- . . . . . . . . . . # . . . . . . . # . . . # . . . . . . . . . . . . . .
- A A A A A . . . . . # . . . . . . . B . . . # # # # # # # # # # # # # . .
- A . . . A . . . . . # . . . . . . . # . . . . . . . . . . . . . . . # . .
- A . . . A . . . . . # . . . . . . . B . . . . . . . . . . . . . . . # . .
- A . . . A . . . . . # . . . . . . . # . . . . . . . . . . . . . . . # . .
- A . . . A . . . . . # . . . . . . . B B B B B B B B B B B B B . . . # . .
- A . . . . . . . . . # . . . . . . . . . . . . . . . . . . . # . . . # . .
- A A A A A A A A A A B . . . . . . . . . . . . . . . . . . . # . . . # . .
- 1 . . . . . . . .10 . . . . . . . . . . . . . . . . . . . . # . . . # . .
- . . . . . . . . . . . . . . . . . . . . . . . . # # # # # # # . . . # . .
- . . . . . . . . . . . . . . . . . . . . . . . . # . . . . . . . . . # . .
- . . . . . . . . . . . . . . . . . . . . . . A A A A A A A A A A A . # . .
- . . . . . . . . . . . . . . . . . . . . . . # . # . . . . . . . A . # . .
- . . . . . . . . . . . . . . . . . . . . . . # . # . . . A # # # A # # . .
- . . . . . . . . . . . . . . . . . . . . . . # . # . . . A . . . A . . . .
- . . . . . . . . . . . . . . . . . . . . . . # # # # # # A # # # O A # . .
- . . . . . . . . . . . . . . . . . . . . . . . . # . . . A . . . A . # . .
- . . . . . . . . . . . . . . . . . . . . . . . . # . . . A A A A A . # . .
- . . . . . . . . . . . . . . . . . . . . . . . . # . . . . . . . . . # . .
- . . . . . . . . . . . . . . . . . . . . . . . . # . . . . . . . . . # . .
- . . . . . . . . . . . . . . . . . . . . . . . . # . . . . . . . . . # . .
- . . . . . . . . . . . . . . . . . . . . . . . . B B B B B B B B B B B B #
- . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . # . A
- . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . # . A
- . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . # . A
- . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . # . A
- . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . # . A
- . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . A A A A A . A
- . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . A . . . . . A
- . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . A . . . . . A
- . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . A . . . . . A
- . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . A A A A A A A
- */
+function zip(input) {
+  const string = input + ",";
+  const matches = string.match(ZIP_REGEXP);
+  let [_, A, B, C] = matches;
+  [A, B, C] = [A, B, C].map(partial => partial.replace(/,$/, "").trim());
+
+  const zipped = string
+    .replace(new RegExp(A, "g"), "A")
+    .replace(new RegExp(B, "g"), "B")
+    .replace(new RegExp(C, "g"), "C")
+    .replace(/,$/, "");
+
+  return [zipped, A, B, C];
+}
+
+function dust(program) {
+  const codes = parseInput(program);
+
+  let output = "";
+  new Computer(codes).run(
+    () => {},
+    char => {
+      output += String.fromCharCode(char);
+    }
+  );
+
+  const scaffold = new Scaffold(output);
+  const route = scaffold.traverse();
+
+  const input = [...zip(route), "n"].join("\n") + "\n";
+
+  codes[0] = 2;
+  let dust = 0;
+  let char = 0;
+  new Computer(codes).run(
+    () => {
+      return input[char++].charCodeAt(0);
+    },
+    output => {
+      dust = output;
+    }
+  );
+
+  return dust;
+}
+
+module.exports = { alignment, dust };
--- day17/scaffold.test.js
+++ day17/scaffold.test.js
@@ -1,10 +1,10 @@
 const assert = require("assert");
 
-const { alignment } = require("./scaffold");
+const { alignment, dust } = require("./scaffold");
 const { fetchInput } = require("../utils");
 
 suite("Day 17: Set and Forget", () => {
-  suite.only("Part 1", () => {
+  suite("Part 1", () => {
     const expectedAnswer = 5724;
     test(`Answer is ${expectedAnswer}`, () => {
       const input = fetchInput();
@@ -13,10 +13,10 @@ suite("Day 17: Set and Forget", () => {
   });
 
   suite("Part 2", () => {
-    const expectedAnswer = 398;
+    const expectedAnswer = 732985;
     test(`Answer is ${expectedAnswer}`, () => {
       const input = fetchInput();
-      assert.equal(drone(input).time, expectedAnswer);
+      assert.equal(dust(input), expectedAnswer);
     });
   });
 });
