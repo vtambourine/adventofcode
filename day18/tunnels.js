32db596057d8a0f8cc51a66803490ccfa0a68f90
Benjamin T <vtambourine@ya.ru>
2020-01-07 22:55:35 +0100

    Day 18. Draft

--- day18/tunnels.js
+++ day18/tunnels.js
@@ -1,9 +1,9 @@
 function parseInput(input) {
-  console.log(input);
   return input.split("\n").map(row => [...row]);
 }
 
 const ENTRANCE = "@";
+const WALL = "#";
 
 const DIRECTIONS = [
   [0, -1],
@@ -20,12 +20,90 @@ function isDoor(symbol) {
   return "A" <= symbol && symbol <= "Z";
 }
 
+function reachable(map, start, availableKeys = []) {
+  const keys = {};
+  const visited = { [String(start)]: 0 };
+  const queue = [start];
+  while (queue.length) {
+    const [x, y] = queue.shift();
+    for (let [dx, dy] of DIRECTIONS) {
+      const [xp, yp] = [x + dx, y + dy];
+
+      if (xp < 0 || xp >= map[0].length || yp < 0 || yp >= map.length) {
+        continue;
+      }
+
+      const point = String([xp, yp]);
+
+      if (visited[point]) {
+        continue;
+      }
+
+      visited[point] = visited[String([x, y])] + 1;
+
+      const symbol = map[yp][xp];
+      if (symbol === WALL) {
+        continue;
+      }
+
+      if (isKey(symbol) && !availableKeys.includes(symbol)) {
+        keys[symbol] = [visited[point], [xp, yp]];
+      }
+
+      if (isDoor(symbol) && !availableKeys.includes(symbol.toLowerCase())) {
+        continue;
+      }
+
+      queue.push([xp, yp]);
+    }
+  }
+
+  return keys;
+}
+
+const memo = {};
+function walk(map, start, availableKeys) {
+  const sortedKeys = availableKeys.sort();
+  const keys = reachable(map, start, sortedKeys.slice());
+
+  if (memo[String(start.concat(sortedKeys))]) {
+    // console.log("found!");
+    return memo[String(start.concat(sortedKeys))];
+  }
+
+  // if (Object.keys(memo).length % 10) {
+  // console.log("cache size", Object.keys(memo).length);
+  // }
+
+  let ans = 0;
+  // console.log("walk form", start, "with", availableKeys, "got", keys);
+  if (Object.keys(keys).length === 0) {
+    ans = 0;
+  } else {
+    let ndist = [];
+    for (let [k, [dist, nstart]] of Object.entries(keys)) {
+      const nw = walk(map, nstart, sortedKeys.concat(k).sort());
+      // console.log(".. reach for", k, dist, "aways", "=", nw);
+      ndist.push(dist + nw);
+    }
+
+    ans = ndist.sort().shift();
+    // console.log(ans);
+
+    memo[String(start.concat(sortedKeys))] = ans;
+  }
+
+  // console.log(memo);
+
+  return ans;
+}
+
 function path(input) {
   const map = parseInput(input);
   const width = map[0].length;
   const height = map.length;
 
-  console.log({ width, height });
+  // console.log({ width, height });
 
   const xy2i = ([x, y]) => y * width + x;
   const i2xy = i => [i % width, Math.floor(i / width)];
@@ -39,47 +117,14 @@ function path(input) {
     }
   }
 
-  console.log({ x, y });
+  // console.log({ x, y });
 
-  function reachable(map, [x, y], hasKeys = ["a"]) {
-    const queue = [[x, y]];
-    const visited = { [xy2i([x, y])]: 0 };
-    const reach = {};
-    while (queue.length) {
-      const [a, b] = queue.shift();
-      for (let [dx, dy] of DIRECTIONS) {
-        const [nx, ny] = [a + dx, b + dy];
-        if (nx < 0 || nx >= width || ny < 0 || ny >= height) {
-          continue;
-        }
+  // reachable(map, [x, y]);
 
-        const point = xy2i([nx, ny]);
-        if (visited[point]) {
-          continue;
-        } else {
-          visited[point] = visited[xy2i([a, b])] + 1;
-        }
-        const symbol = map[ny][nx];
-        if (symbol === "#") {
-          continue;
-        }
-        if (isKey(symbol)) {
-          reach[symbol] = visited[point];
-        }
-        if (isDoor(symbol) && !hasKeys.includes(symbol.toLowerCase())) {
-          continue;
-        }
-        queue.push([nx, ny]);
-      }
-    }
+  const w = walk(map, [x, y], []);
+  // console.log(w);
 
-    // console.log(visited);
-    console.log(reach);
-  }
-
-  reachable(map, [x, y]);
-
-  return -1;
+  return w;
 }
 
 module.exports = { path };
--- day18/tunnels.test.js
+++ day18/tunnels.test.js
@@ -3,11 +3,11 @@ const assert = require("assert");
 const { path } = require("./tunnels");
 const { fetchInput } = require("../utils");
 
-suite("Day 17: Set and Forget", () => {
+suite("Day 18: Many-Worlds Interpretation", () => {
   suite("Part 1", () => {
     test("Test cases are valid", () => {
       [
-        // ["#########\n" + "#b.A.@.a#\n" + "#########", 8],
+        ["#########\n" + "#b.A.@.a#\n" + "#########", 8],
         [
           "########################\n" +
             "#f.D.E.e.C.b.A.@.a.B.c.#\n" +
@@ -15,6 +15,35 @@ suite("Day 17: Set and Forget", () => {
             "#d.....................#\n" +
             "########################",
           86,
+        ],
+        [
+          "########################\n" +
+            "#...............b.C.D.f#\n" +
+            "#.######################\n" +
+            "#.....@.a.B.c.d.A.e.F.g#\n" +
+            "########################",
+          132,
+        ],
+        [
+          "########################\n" +
+            "#@..............ac.GI.b#\n" +
+            "###d#e#f################\n" +
+            "###A#B#C################\n" +
+            "###g#h#i################\n" +
+            "########################",
+          81,
+        ],
+        [
+          "#################\n" +
+            "#i.G..c...e..H.p#\n" +
+            "########.########\n" +
+            "#j.A..b...f..D.o#\n" +
+            "########@########\n" +
+            "#k.E..a...g..B.n#\n" +
+            "########.########\n" +
+            "#l.F..d...h..C.m#\n" +
+            "#################",
+          136,
         ],
       ].forEach(([input, expected]) => {
         assert.equal(path(input), expected);
@@ -28,5 +57,11 @@ suite("Day 17: Set and Forget", () => {
     });
   });
 
-  suite("Part 2", () => {});
+  suite.skip("Part 2", () => {
+    const expectedAnswer = 0;
+    test(`Answer is ${expectedAnswer}`, () => {
+      const input = fetchInput();
+      assert.equal(path(input), expectedAnswer);
+    });
+  });
 });
