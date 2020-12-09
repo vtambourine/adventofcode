48a3a47f5d75c2a71821c558be07a756b4424b5d
Benjamin T <vtambourine@ya.ru>
2020-12-09 21:20:58 +0300

    2020. Day 2: Password Philosophy

--- 2020/day02.password_philosophy/day02.go
+++ 2020/day02.password_philosophy/day02.go
@@ -1,51 +1,16 @@
 package day02
 
 import (
-	"github.com/vtambourine/adventofcode/2020/command"
-	"regexp"
 	"strings"
 
 	"github.com/vtambourine/adventofcode/2020/challenge"
-	"github.com/vtambourine/adventofcode/2020/util"
+	"github.com/vtambourine/adventofcode/2020/command"
 )
 
-type policy struct {
-	char rune
-	min  int
-	max  int
-}
-
-func parsePolicy(line string) policy {
-	re := regexp.MustCompile(`(\d+)-(\d+) ([[:lower:]])`)
-	matches := re.FindAllStringSubmatch(line, -1)
-
-	return policy{
-		char: []rune(matches[0][3])[0],
-		min:  util.MustAtoi(matches[0][1]),
-		max:  util.MustAtoi(matches[0][2]),
-	}
-}
-
-func (p policy) valid(password string) bool {
-	count := 0
-
-	for _, r := range password {
-		if r == p.char {
-			count++
-		}
-	}
-
-	return count >= p.min && count <= p.max
-}
-
-func (p policy) strictlyValid(password string) bool {
-	return (rune(password[p.min-1]) == p.char) != (rune(password[p.max-1]) == p.char)
-}
-
-func Policy(c *challenge.Challenge) int {
+func validPassports(input *challenge.Challenge) int {
 	valid := 0
 
-	for line := range c.Lines() {
+	for line := range input.Lines() {
 		parts := strings.Split(line, ": ")
 		if parsePolicy(parts[0]).valid(parts[1]) {
 			valid++
@@ -55,10 +20,10 @@ func Policy(c *challenge.Challenge) int {
 	return valid
 }
 
-func NewPolicy(c *challenge.Challenge) int {
+func strictlyValidPassports(input *challenge.Challenge) int {
 	valid := 0
 
-	for line := range c.Lines() {
+	for line := range input.Lines() {
 		parts := strings.Split(line, ": ")
 		if parsePolicy(parts[0]).strictlyValid(parts[1]) {
 			valid++
@@ -69,6 +34,6 @@ func NewPolicy(c *challenge.Challenge) int {
 }
 
 func Register(s command.Solutions) {
-	s.Register(2, "a", Policy)
-	s.Register(2, "b", NewPolicy)
+	s.Register(2, "a", validPassports)
+	s.Register(2, "b", strictlyValidPassports)
 }
--- 2020/day02.password_philosophy/day02_test.go
+++ 2020/day02.password_philosophy/day02_test.go
@@ -1,35 +1,36 @@
 package day02
 
 import (
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/vtambourine/adventofcode/2020/challenge"
-	"testing"
 )
 
 var example = `1-3 a: abcde
 1-3 b: cdefg
 2-9 c: ccccccccc`
 
-func TestPolicy(t *testing.T) {
+func TestValidPassports(t *testing.T) {
 	input := challenge.ReadChallengeFromLiteral(example)
-	got := Policy(input)
+	got := validPassports(input)
 	assert.Equal(t, 2, got)
+}
+
+func TestStrictlyValidPassports(t *testing.T) {
+	input := challenge.ReadChallengeFromLiteral(example)
+	got := strictlyValidPassports(input)
+	assert.Equal(t, 1, got)
 }
 
 func TestPartOne(t *testing.T) {
 	input := challenge.ReadChallengeFromFile("./day02.input")
-	got := Policy(input)
+	got := validPassports(input)
 	assert.Equal(t, 456, got)
-}
-
-func TestNewPolicy(t *testing.T) {
-	input := challenge.ReadChallengeFromLiteral(example)
-	got := NewPolicy(input)
-	assert.Equal(t, 1, got)
 }
 
 func TestPartTwo(t *testing.T) {
 	input := challenge.ReadChallengeFromFile("./day02.input")
-	got := NewPolicy(input)
+	got := strictlyValidPassports(input)
 	assert.Equal(t, 308, got)
 }
